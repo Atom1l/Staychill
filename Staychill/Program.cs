dcce11dac930efdf7b3efc0aaff99ce34bd602b5
atom <atom7548@gmail.com>
2024-11-10 04:42:39 +0700

    Update Cart-Frontend

--- Staychill/Program.cs
+++ Staychill/Program.cs
@@ -33,6 +33,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=TestUserCreatingAccount}/{action=Index}/{id?}");
+    pattern: "{controller=Cart}/{action=CartIndex}/{id?}");
 
 app.Run();
